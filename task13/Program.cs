525d153c07c265ac21bf612f7b2003846b3c9dab
Mikhail <kostrominmi@mail.ru>
2022-08-23 23:12:32 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,26 @@
+﻿int x = new Random().Next(0,99999);
+Console.WriteLine($"сгенерировалось случайное число {x}");
+if (x <=99)
+{
+    Console.WriteLine("Нет второго числа");
+}
+else if (x>=100)
+if (x <=999)
+{
+Console.WriteLine (x=x%10);
+}
+
+
+else if (x>=1000)
+if (x <=9999)
+{
+Console.WriteLine (x=x/10 %10);
+}
+
+else if (x>=10000)
+if (x <=99999)
+{
+Console.WriteLine (x=x/100 %10);
+}
+
+

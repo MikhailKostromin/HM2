5eade4da0c50a8402ee4e6b88be9632828f1fb4d
Mikhail <kostrominmi@mail.ru>
2022-08-24 00:16:33 +0300

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите цифру от 1 до 7");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x==6)
+{
+    Console.WriteLine("Выходной!");
+}
+else if (x==7)
+{
+    Console.WriteLine("Выходной!");
+}
+else 
+{
+    Console.WriteLine("Рабочий!");
+}

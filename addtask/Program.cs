52521d6b37d29bc6ea88f30bdbdf7510035c18f6
Mikhail <kostrominmi@mail.ru>
2022-08-24 21:35:19 +0300

    addtask

---
+++ addtask/Program.cs
@@ -0,0 +1,24 @@
+﻿int x = new Random().Next(0,1000);
+Console.WriteLine($"сгенерировалось случайное количество программистов {x}");
+
+if (x % 10== 1)
+{
+     Console.WriteLine($"{x} программист");
+}
+
+if (x % 10 > 1)
+{
+    if (x % 10 < 5)
+    {
+         Console.WriteLine($"{x} программиста");
+    }
+}
+
+if (x % 10 >= 5)
+{
+    Console.WriteLine($"{x} программистов"); 
+}
+if (x % 10 == 0)
+{
+    Console.WriteLine($"{x} программистов");
+}
